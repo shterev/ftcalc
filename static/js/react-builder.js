065fb22039debd546170653ca3a055cce6eff8d1
Yordan Shterev <shterew@gmail.com>
2023-01-25 14:50:30 +0200

    Updates

---
+++ static/js/react-builder.js
@@ -0,0 +1,23 @@
+(function () {
+  var reactFiles = 'static/css/main.073c9b0a.css,static/js/main.1feb6817.js';
+  var reactFiles = reactFiles.split(',');
+  var reactFilesSize = reactFiles.length;
+  var isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
+
+  if(isIE11) {
+    var IEscriptTag = document.createElement('script');
+    IEscriptTag.setAttribute(
+      'src',
+      'https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/7.12.1/polyfill.min.js'
+    );
+    document.body.appendChild(IEscriptTag);
+  }
+
+  for (var i = 0; i < reactFilesSize; i++) {
+    var scriptTag = document.createElement('script');
+    scriptTag.setAttribute(
+      'src', './' + reactFiles[i]
+    );
+    document.body.appendChild(scriptTag);
+  }
+})();
