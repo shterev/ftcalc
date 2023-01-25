e65d4cb0d75d55d0f7aae1acf53da50d33eb29be
Yordan Shterev <shterew@gmail.com>
2023-01-25 10:57:46 +0200

    Updates

---
+++ precache-manifest.f7e77bdd45305f36e109c4012ed60aff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9289ec66d2ca6f420206",
+    "url": "/ftcalc/static/css/main.507def8e.chunk.css"
+  },
+  {
+    "revision": "9289ec66d2ca6f420206",
+    "url": "/ftcalc/static/js/main.9289ec66.chunk.js"
+  },
+  {
+    "revision": "6154548a7bf39ac6e595",
+    "url": "/ftcalc/static/js/1.6154548a.chunk.js"
+  },
+  {
+    "revision": "6fcf5553061dca5a56e6",
+    "url": "/ftcalc/static/js/2.6fcf5553.chunk.js"
+  },
+  {
+    "revision": "afa78bcb4960e855a8cd",
+    "url": "/ftcalc/static/js/runtime~main.afa78bcb.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/ftcalc/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "9a2368840e39eb4519b9e75eb0f800cd",
+    "url": "/ftcalc/index.html"
+  }
+];
